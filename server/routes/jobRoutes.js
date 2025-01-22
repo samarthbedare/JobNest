8103cdf9f999eb2c0724a1ef7d8d48461455a05d
Aditi Apte <aditiapte15@gmail.com>
2025-01-22 12:47:35 +0530

    Revert "Server Setup Complted"
    This reverts commit c29025e6ddec857ffed410effd6fde3455240a29.

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,28 +1,10 @@
 import express from "express";
-import {createJob,getJobs ,getJobsByUser,searchJobs,applyJobs,likeJob,getJobById,deleteJob,}from "../controllers/jobController.js"
+import {createJob,getJobs ,getJobsByUser}from "../controllers/jobController.js"
 import protect from "../middleware/protect.js";
-const router = express.Router()
+const router = express.Router();
 
 router.post("/jobs",protect,createJob);
 router.get("/jobs",getJobs);
-router.get("/jobs/user/:id",protect,getJobsByUser);
-//search Jobs
-router.get("/jobs/search",searchJobs);
-
-//apply for job
-router.put("/jobs/apply/:id",protect,applyJobs);
-
-// Like job and unlike job
-router.put("/jobs/like/:id",protect,likeJob)
-
-//getJobById
-router.get("/jobs/:id",protect,getJobById);
-
-//delete job
-router.delete("/jobs/:id",protect,deleteJob);
-
-
-
-
+router.get("/jobs/user/:id",getJobsByUser);
 
 export default router;
